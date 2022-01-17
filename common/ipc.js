e465b7a20c66d85a8ec9b16106666157d1d731d4
terry <jstel@126.com>
2022-01-17 21:02:45 +0800

    init

--- common/ipc.js
+++ common/ipc.js
@@ -10,12 +10,14 @@ ipcMain.on('file-message', (event, args) => {
     console.log(ffmpeg.path);
     console.log(ffprobe.path);
     console.log(ffprobeInstaller.path, ffprobeInstaller.version)
-
-    let _cmd = ffprobe.path + "  -print_format json -show_entries stream_tags:format_tags  " + JSON.stringify(args.path)
+    var _cmd
+    // _cmd = ffprobe.path + "  -print_format json -show_entries stream_tags:format_tags  " + JSON.stringify(args.path)
+    // _cmd = JSON.stringify(ffmpeg.path) + "  -i  " + JSON.stringify(args.path)
+    _cmd = JSON.stringify(ffprobe.path) + " -v error -of json " + JSON.stringify(args.path) + " -of json -show_entries \"stream=index:stream_tags=language\" -select_streams s"
     console.log(_cmd)
 
     let _r = execSync(_cmd)
 
-    console.log(_r)
+    console.log(_r.toString())
 
 })
