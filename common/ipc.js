1c0e92cb4b6364abd2dbfd3ddea52aad16486c15
terry <jstel@126.com>
2022-01-17 20:51:31 +0800

    init

--- common/ipc.js
+++ common/ipc.js
@@ -3,16 +3,19 @@ const ffmpeg = require('ffmpeg-static-electron')
 var ffprobe = require('ffprobe-static')
 const ffprobeInstaller = require('@ffprobe-installer/ffprobe')
 
+const {exec, execSync} = require('child_process')
+
 ipcMain.on('file-message', (event, args) => {
     console.log(args)
     console.log(ffmpeg.path);
     console.log(ffprobe.path);
     console.log(ffprobeInstaller.path, ffprobeInstaller.version)
 
-    var track = args.path
-    ffprobe(track).then(r=>{
-        console.log(track)
-    }).catch(e=>{
-        console.log(e)
-    })
+    let _cmd = ffprobe.path + "  -print_format json -show_entries stream_tags:format_tags  " + JSON.stringify(args.path)
+    console.log(_cmd)
+
+    let _r = execSync(_cmd)
+
+    console.log(_r)
+
 })
